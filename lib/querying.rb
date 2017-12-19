8c5c6897808c00bfa34102d76b7802835562a0b8
Yomaira Escano <yjescano@gmail.com>
2017-12-19 03:47:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,31 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
+  "SELECT title, year
+  FROM books
+  WHERE series_id = 1
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters WHERE name = 'Tyrion Lannister';"
+  "SELECT name, motto
+  FROM characters
+  WHERE name = 'Tyrion Lannister';"
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name AS 'authors_name', subgenres.name AS 'subgenres_name' FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
+  "SELECT authors.name AS 'authors_name', subgenres.name AS 'subgenres_name'
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
