78945c6ddb029180452d81ba8b283ec4c9979c30
Yomaira Escano <yjescano@gmail.com>
2017-12-19 04:37:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,15 +29,20 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title 
-  FROM series 
-  JOIN characters 
-  ON series.id = characters.series_id 
-  WHERE characters.species = 'human' 
-  GROUP BY series.title 
+  "SELECT series.title
+  FROM series
+  JOIN characters
+  ON series.id = characters.series_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
   LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC;"
+  "SELECT characters.name, COUNT(character_books.book_id)
+  FROM characters
+  JOIN character_books
+  ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_books.book_id) DESC;"
 end
