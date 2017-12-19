d0065c18dda4deab6af1f6aa760248227f1b64d0
Yomaira Escano <yjescano@gmail.com>
2017-12-19 02:28:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,6 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters WHERE name = 'Tyrion Lannister';"
 end
 
-
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
 end
