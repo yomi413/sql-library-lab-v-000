beff1ba6ef53818cf9606134285155bd918f54ac
Yomaira Escano <yjescano@gmail.com>
2017-12-19 02:51:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT * FROM subgenres"
 end
 
 def select_series_title_with_most_human_characters
