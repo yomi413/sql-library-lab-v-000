f36f5657118a92f18d2e430fddee39441a47b7a0
Yomaira Escano <yjescano@gmail.com>
2017-12-19 04:37:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY series.title LIMIT 1;"
+  "SELECT series.title 
+  FROM series 
+  JOIN characters 
+  ON series.id = characters.series_id 
+  WHERE characters.species = 'human' 
+  GROUP BY series.title 
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
