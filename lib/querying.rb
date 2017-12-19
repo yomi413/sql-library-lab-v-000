334985ea9e87a4d47f39a47a7ccb4e6b0e8f629d
Yomaira Escano <yjescano@gmail.com>
2017-12-19 02:30:39 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT * FROM characters;"
 end
 
 def select_name_and_series_subgenres_of_authors
